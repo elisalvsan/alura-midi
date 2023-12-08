3fbbe89c03d7a8688ef95d0b39489126a861861f
Elisa Andrade <elisalvsan@gmail.com>
2023-12-08 19:27:50 -0300

    reorganizando o codigo

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,16 @@
-function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
 
-while( contador < listaDeTeclas.length){
-    
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom(idAudio);  
-    };
-    contador++;
+  listaDeTeclas[contador].onclick = function () {
+    tocaSom(idAudio);
+  };
 }
